e173634d688a9e9f74e0a494fc31e23b63a9b15e
Christian Delgado <cdelgado@gmail.com>
2024-06-07 09:17:42 -0500

    first commit

---
+++ myCalculator.py
@@ -0,0 +1,11 @@
+#Calculadora desarrollada por Christian Delgado
+
+while True:
+     print("""Opciones
+    Ingrese '+' para sumar dos numeros
+    Ingrese '-' para restar dos numeros
+    Ingrese '*' para multiplicar dos numeros
+    Ingrese '/' para dividir dos numeros
+    Ingrese 'salir' para finalizar""")
+
+    #teclado=input(":")
